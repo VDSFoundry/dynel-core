165554331a2f6d8e7f19156f34ab83d8be5f05df
geraldf <geraldf@visualdatasolutions.com>
2014-09-03 01:39:26 -0400

    Completed first batch of tests for EventSource

---
+++ test/eventsource-test.js
@@ -0,0 +1,32 @@
+// Copyright (c) Visual Data Solutions, Inc. All rights reserved. This source code is distributed under the terms of the MIT license. See LICENSE file in the project root for complete license information.
+
+var chai = require('chai');
+var expect = chai.expect;
+var EventSource = require('../lib/eventsource.js');
+
+
+describe('EventSource', function() {
+    this.timeout(500);
+
+    var source;
+    beforeEach(function() {
+        source = new EventSource();
+    });
+
+    it('should fire the event callback when emit is called', function(done) {
+        source.on('event', function() {
+            done();
+        });
+        source.emit('event');
+    });
+
+    it ('should forward the data passed to emit to the event callback', function(done) {
+        source.on('event', function(data) {
+            if (data === 'data') {
+                done();
+            }
+        });
+        source.emit('event', 'data');
+    });
+
+});
