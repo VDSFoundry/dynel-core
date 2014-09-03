b349f5b8a12061069ed59854a9e4f44cab8d8a82
geraldf <geraldf@visualdatasolutions.com>
2014-09-02 21:14:57 -0400

    completed first batch of tests for Extend

--- test/coreobject-test.js
+++
@@ -1,1 +0,0 @@
-// Copyright (c) Visual Data Solutions, Inc. All rights reserved. This source code is released under an MIT license. See LICENSE file in the project root for complete license information.
--- test/extend-test.js
+++ test/extend-test.js
@@ -1,35 +1,52 @@
 // Copyright (c) Visual Data Solutions, Inc. All rights reserved. This source code is released under an MIT license. See LICENSE file in the project root for complete license information.
 
-//var assert = require('assert');
-//var chai = require('chai');
-//var assert = chai.assert;
 var chai = require('chai');
+var expect = chai.expect;
 
-chai.should();
-
-
-var extend = require('../lib/extend.js');
-
+var Extend = require('../lib/extend.js');
 
 describe('Extend', function()
 {
     var baseObject;
+    var derivedObject;
 
     beforeEach(function() {
 
-        baseOject = {
+        baseObject = {
             Value1: 'value1'
-        }
+        };
+
+        derivedObject = {
+            Value2: 'value2'
+        };
     });
 
-    it('derived object should have Value1 field from base object', function()
+    it('should have property Value1 from base object', function()
     {
-        var derivedObject = {};
+        Extend(derivedObject, baseObject);
 
-        extend(derivedObject, baseOject);
+        expect(derivedObject).to.have.property('Value1');
+        expect(derivedObject.Value1).to.equal('value1');
+    });
 
-        derivedObject.should.have.property('Value1');
+    it('should still have Value2 property', function()
+    {
+        Extend(derivedObject, baseObject);
+
+        expect(derivedObject).to.have.property('Value2');
+        expect(derivedObject.Value2).to.equal('value2');
     });
 
 
+    it ('should throw Error if base object is null or undefined', function()
+    {
+        expect(Extend.bind(null, baseObject)).to.throw(Error);
+        expect(Extend.bind(undefined, baseObject)).to.throw(Error);
+    });
+
+    it ('should throw Error if derived object is null or undefined', function()
+    {
+        expect(Extend.bind(derivedObject, null)).to.throw(Error);
+        expect(Extend.bind(derivedObject, undefined)).to.throw(Error);
+    });
 })
