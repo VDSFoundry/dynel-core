38a9c7e5c94a0a641b1156dc8545bc034d0ea03d
geraldf <geraldf@visualdatasolutions.com>
2014-09-04 19:58:29 -0400

    Added gruntfile.js and some cleanup

--- test/moduleregistry-test.js
+++ test/moduleregistry-test.js
@@ -1,5 +1,7 @@
 // Copyright (c) Visual Data Solutions, Inc. All rights reserved. This source code is distributed under the terms of the MIT license. See LICENSE file in the project root for complete license information.
 
+/*jshint expr: true*/
+
 /*
 Module registry
 ---------------
