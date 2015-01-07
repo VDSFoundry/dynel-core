1fe321c07b00922e6a55b1d9afe5224bd12dd337
Gerald Fishel <geraldf@visualdatasolutions.com>
2015-01-07 16:44:11 -0500

    added index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+// Copyright (c) Visual Data Solutions, Inc. All rights reserved. This source code is distributed under the terms of the MIT license. See LICENSE file in the project root for complete license information.
+
+module.exports = {
+    App: require('./lib/app.js'),
+    DynObject: require('./lib/dynobject.js'),
+    EventSource: require('./lib/eventsource.js'),
+    Module: require('./lib/module.js'),
+    ModuleRegistry: require('./lib/moduleregistry.js')
+};
